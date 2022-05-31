c79b48ff95e98482b765106d092ebc2421a7693a
Gabinozor-dev <dylanhouche@gmail.com>
2022-05-31 23:40:07 +0200

    premier commit

---
+++ django-app/photoshare/photos/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from .models import Category, Photo
+
+
+# Create your views here.
+
+
+def gallery(request):
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories': categories, 'photos': photos}
+    return render (request, 'photos/gallery.html', context)
+
+
+def viewphotos(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render (request, 'photos/photos.html', {'photo': photo})
+
+
+
+def addphotos(request):
+    categories = Category.objects.all()
+    
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+         
+        if data['category'] != 'none':
+            category = Category.objects.get(id = data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+            
+        photo = Photo.objects.create(
+            category = category,
+            description = data['description'],
+            image = image,
+        )
+        
+        return redirect('gallery')
+              
+    context = {'categories': categories}
+    return render (request, 'photos/add.html', context)
